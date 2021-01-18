ec32bc6ac9893d42016abb5c95ffed1ec0a2821f
Tarskan <55380090+Tarskan@users.noreply.github.com>
2021-01-18 18:02:25 +0100

    Barre de recherche

--- public/template/header.php
+++ public/template/header.php
@@ -19,8 +19,13 @@
         <li class="nav-item">
           <a class="nav-link" href="http://quizjapan/vue/bibliotheque">Bibliothèque</a>
         </li>
+      </ul>
+      <ul class="navbar-nav mr-auto">
         <li class="nav-item">
-          <a class="nav-link" href="http://quizjapan/vue/vueErreur">Si un lien ne marche pas clique moi</a>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
         </li>
       </ul>
 <!--  /////////////////////////////////////////////Connexion/deconnexion/////////////////////////////////////////////////// -->
