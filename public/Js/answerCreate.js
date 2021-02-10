96f4b490de7bbeb23d34e5a8c288a4fa8cc913f9
Tarskan <55380090+Tarskan@users.noreply.github.com>
2021-02-10 10:28:26 +0100

    Correction js

--- public/Js/answerCreate.js
+++ public/Js/answerCreate.js
@@ -1,30 +1,13 @@
 var add = document.getElementById('addQuestion');
-let contenu = "<div class='form-row'>";
-contenu +="<div class='form-group col'>";
-contenu +="<label for='name'>Question</label>";
-contenu +="<input type='text' class='form-control' id='question' name='question' required>";
-contenu +="</div>";
-contenu +="</div>"
-contenu +="<div class='form-row'>"
-contenu +="<div class='form-group col'>";
-contenu +="<label for='name'>Bonne réponse</label>";
-contenu +="<input type='text' class='form-control' id='goodAnswer' name='goodAnswer' required>";
-contenu +="</div>";
-contenu +="</div>";
-contenu +="<div class='form-row'>";
-contenu +="<div class='form-group col'>";
-contenu +="<label for='name'>Mauvaise réponse</label>";
-contenu +="<input type='text' class='form-control' id='badAnswer' name='badAnswer' required>";
-contenu +="</div>";
-contenu +="</div>";
+let contenu = "";
 var compteurQuestionFausse = 1;
             
 add.addEventListener('click', event => {
     event.preventDefault();
     compteurQuestionFausse++;
-    $('#questionaire').empty();
+    // $('#questionaire').empty();
     if(compteurQuestionFausse <= 5){
-        contenu +="<div class='form-row'>";
+        contenu ="<div class='form-row'>";
         contenu +="<div class='form-group col'>";
         contenu +="<label for='name'>Mauvaise réponse supplémentaire</label>";
         contenu +="<input type='text' class='form-control badAnswer' name='badAnswer[]' required>";
